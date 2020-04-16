424db8cd9b611c7602592f7c62e0ce54add9f377
Andrea Luzzardi <aluzzardi@gmail.com>
2020-04-15 17:13:26 -0700

    bump bl to 14e94cc5

--- bl.rb
+++ bl.rb
@@ -2,9 +2,9 @@ class Bl < Formula
   homepage "https://blocklayerhq.com"
   desc "Blocklayer CLI"
 
-  version "9c2a9b45"
+  version "14e94cc5"
   url "https://blocklayer-dev.s3.amazonaws.com/alpha-3/cli/#{version}/bl-Darwin-x86_64"
-  sha256 "266f8b8addb11c7fc3bd1e02e82b53130c2480c5e0b788abf10d3af49e0d3d21"
+  sha256 "7208fd452de0633c398e02f49d16fdda93ee7a93d9de53c5dfd4038dc2536a75"
 
   def install
     mv "bl-Darwin-x86_64", "bl"
