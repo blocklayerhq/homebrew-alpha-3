f73e7c4148dda1fefde96efa5b05e0b4472bc4da
Sam Alba <sam.alba@gmail.com>
2020-06-03 17:43:59 -0700

    bump bl formula version to 20200603.1 (cli version 46498d44)

--- bl.rb
+++ bl.rb
@@ -2,11 +2,11 @@ class Bl < Formula
   homepage "https://blocklayerhq.com"
   desc "Blocklayer CLI"
 
-  version "20200527.1"
+  version "20200603.1"
   version_scheme 1
 
-  url "https://blocklayer-dev.s3.amazonaws.com/alpha-3/cli/0b507c12/bl-Darwin-x86_64"
-  sha256 "9dc8a5f71967a613a59445e9c3f81f2271b2de98b7492db4852aef8d9cb67e75"
+  url "https://blocklayer-dev.s3.amazonaws.com/alpha-3/cli/46498d44/bl-Darwin-x86_64"
+  sha256 "f0a646eb9b2c8932a500fda0316fb3265cc49dec798dd2042d3583a9301b3b5b"
 
   def install
     mv "bl-Darwin-x86_64", "bl"
